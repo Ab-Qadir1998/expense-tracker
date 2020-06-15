a7fbd03ee5281f89917ab47a8ab4df74aa03d694
Ab-Qadir1998 <abdulqadir12356@gmail.com>
2020-06-16 02:55:31 +0500

    Adding new expense tracker app

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h5>Your Balance</h5>
+            <h1 id="balance">$0.0</h1>
+        </>
+    )
+}
