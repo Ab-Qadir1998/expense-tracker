d70470b6a0b6ee54749c65ca26c4aa0e378e061f
Ab-Qadir1998 <abdulqadir12356@gmail.com>
2020-06-19 21:57:34 +0500

    updated files

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import GlobalState, { GlobalContext } from '../context/GlobalState';
+import { GlobalContext } from '../context/GlobalState';
 
 
 export const Balance = () => {
